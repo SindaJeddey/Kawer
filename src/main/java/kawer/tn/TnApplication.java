30dfe5c0a290e704eb8ed453729e7291f759fbdf
SindaJeddey <sindajeddey@gmail.com>
2020-07-05 12:33:44 +0200

    Project initialization

---
+++ src/main/java/kawer/tn/TnApplication.java
@@ -0,0 +1,13 @@
+package kawer.tn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TnApplication.class, args);
+    }
+
+}
